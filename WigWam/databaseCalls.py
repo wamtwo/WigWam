3375c58eed24aac937a4467ab114654f31c9b550
Daniel Fitzek <Daniel.Fitzek@combionic.com>
2019-01-16 19:23:06 +0100

    added writeCharNamesAtOnce tested performance between writeCharNames/WriteCharNamesAtOnce Added counter to writeCharNames

--- WigWam/databaseCalls.py
+++ WigWam/databaseCalls.py
@@ -1,5 +1,6 @@
 import os
-from sqlalchemy import create_engine, Table, select, MetaData, insert
+import time
+from sqlalchemy import create_engine, Table, select, MetaData, insert, delete
 from sqlalchemy.exc import IntegrityError
 
 
@@ -13,9 +14,11 @@ connection = engine.connect()
 def writeCharNames(target_table, namelist):
     table = Table(target_table, metadata, autoload=True, autoload_with=engine)
     countrejected = 0
+    starttime = time.clock()
+    print("Writing to Database Table \"" + target_table + "\"")
 
     with connection.begin() as trans:
-        for tuple in namelist:
+        for index, tuple in enumerate(namelist):
             stmt = insert(table).values(Char_Name=tuple[0], Server_Name=tuple[1])
             try:
                 engine.execute(stmt)
@@ -23,13 +26,59 @@ def writeCharNames(target_table, namelist):
                 trans.rollback()
                 countrejected += 1
                 #raise
-        
+            print(str(index) + "/" + str(len(namelist)), end="")
+            print("\r", end="")
+    
+    elapsed = (time.clock() - starttime)
+    print("Done. \t --- \t %.2f seconds." %elapsed, end="\n\n")
     return "{} entries given. {} entries rejected.".format(len(namelist), countrejected)
 
+
+def writeCharNamesAtOnce(target_table, namelist):
+
+    print("Writing to Database Table \"" + target_table + "\"")
+
+    table = Table(target_table, metadata, autoload=True, autoload_with=engine)
+    entries = []
+    starttime = time.clock()
+    for tuple in namelist:
+        entries.append({"Char_Name":tuple[0], "Server_Name":tuple[1]})
+    stmt = insert(table)
+    try:
+        engine.execute(stmt, entries)
+    except:
+        connection.commit()
+    elapsed = (time.clock() - starttime)
+    print("Done. \t --- \t %.2f seconds." %elapsed, end="\n\n")
+    return("Done.")
+
+def getCharNames(target_table, amount=0):
+    table = Table(target_table, metadata, autoload=True, autoload_with=engine)
+    stmt = select([table.columns.Char_Name, table.columns.Server_Name])
+    if amount > 0: stmt = stmt.limit(amount)
+    result = connection.execute(stmt).fetchall()
+
+    return result
 
 
 if __name__ == "__main__":
     #chrlst = [("Morty", "Server1"), ("Rick", "Server1"), ("Summer", "Server1"), ("Jerry", "Server1"), ("Beth", "Server1")]
     #chrlst = [("Winter", "Server1"), ("Spring", "Server1"), ("Summer", "Server2"), ("Autumn", "Server1"), ("Fall", "Server1"), ("Morty", "Server1"), ("Wubba", "Server1")]
-    chrlst = [("Winter", "Server1"), ("Spring", "Server2"), ("Summer", "Server3"), ("Autumn", "Server2"), ("Fall", "Server1"), ("Morty", "Server3"), ("Wubba", "Server1")]
-    print(writeCharNames("Server_Example", chrlst))
+    #chrlst = [("Winter", "Server1"), ("Spring", "Server2"), ("Summer", "Server3"), ("Autumn", "Server2"), ("Fall", "Server1"), ("Morty", "Server3"), ("Wubba", "Server1")]
+    #print(writeCharNames("Server_Example", chrlst))
+
+    testresult = getCharNames("Server_Malfurion", 1000)
+    testresult_fixed = []
+    for tuple in testresult:
+        tuple_new = (tuple[1], tuple[0])
+        testresult_fixed.append(tuple_new)
+
+    table = Table("Server_Malfurion_copy", metadata, autoload=True, autoload_with=engine)   
+    #stmt = delete(table)
+    #print(writeCharNames("Server_Malfurion_copy", testresult_fixed))
+    stmt = delete(table)
+    connection.execute(stmt)
+    writeCharNames("Server_Malfurion_copy", [("Alfar", "Malfurion")])
+    writeCharNamesAtOnce("Server_Malfurion_copy", testresult_fixed)
+
+    print("done.")
