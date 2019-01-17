88ea678263157dbda64f5a5ab8bcec3717eea66c
Daniel Fitzek <Daniel.Fitzek@combionic.com>
2019-01-17 20:07:59 +0100

    writeNamesAtOnce improvements

--- WigWam/databaseCalls.py
+++ WigWam/databaseCalls.py
@@ -21,9 +21,10 @@ def writeCharNames(target_table, namelist):
         for index, tuple in enumerate(namelist):
             stmt = insert(table).values(Char_Name=tuple[0], Server_Name=tuple[1])
             try:
-                engine.execute(stmt)
+                #engine.execute(stmt)
+                connection.execution_options(autocommit=False).execute(stmt)
             except:
-                trans.rollback()
+                #trans.rollback()
                 countrejected += 1
                 #raise
             print(str(index) + "/" + str(len(namelist)), end="")
@@ -40,17 +41,36 @@ def writeCharNamesAtOnce(target_table, namelist):
 
     table = Table(target_table, metadata, autoload=True, autoload_with=engine)
     entries = []
+    countrejected = 0
     starttime = time.clock()
+
+    comparedata = getCharNames(target_table)
+    compareset = set()
+
+    for row in comparedata:
+        compareset.add((row[0], row[1]))
+
+    
+    namelist_cleaned = []
     for tuple in namelist:
+        if tuple not in compareset: namelist_cleaned.append(tuple)
+        else: countrejected += 1
+
+    for tuple in namelist_cleaned:
         entries.append({"Char_Name":tuple[0], "Server_Name":tuple[1]})
     stmt = insert(table)
-    try:
-        engine.execute(stmt, entries)
-    except:
-        connection.commit()
+
+
+
+    with connection.begin() as trans:
+        try:
+            connection.execute(stmt, entries)
+        except:
+            print("Exception occurred")
+
     elapsed = (time.clock() - starttime)
     print("Done. \t --- \t %.2f seconds." %elapsed, end="\n\n")
-    return("Done.")
+    return("Done. Rejections: " + str(countrejected))
 
 def getCharNames(target_table, amount=0):
     table = Table(target_table, metadata, autoload=True, autoload_with=engine)
@@ -67,18 +87,31 @@ if __name__ == "__main__":
     #chrlst = [("Winter", "Server1"), ("Spring", "Server2"), ("Summer", "Server3"), ("Autumn", "Server2"), ("Fall", "Server1"), ("Morty", "Server3"), ("Wubba", "Server1")]
     #print(writeCharNames("Server_Example", chrlst))
 
-    testresult = getCharNames("Server_Malfurion", 1000)
+    #testresult = getCharNames("Server_Malfurion", 1000)
+    #testresult_fixed = []
+    #for tuple in testresult:
+    #    tuple_new = (tuple[1], tuple[0])
+    #    testresult_fixed.append(tuple_new)
+
     testresult_fixed = []
-    for tuple in testresult:
-        tuple_new = (tuple[1], tuple[0])
-        testresult_fixed.append(tuple_new)
+    for i in range(10000):
+        testresult_fixed.append(("Char_" + str(i), "Server1"))
+
 
     table = Table("Server_Malfurion_copy", metadata, autoload=True, autoload_with=engine)   
-    #stmt = delete(table)
-    #print(writeCharNames("Server_Malfurion_copy", testresult_fixed))
     stmt = delete(table)
     connection.execute(stmt)
-    writeCharNames("Server_Malfurion_copy", [("Alfar", "Malfurion")])
-    writeCharNamesAtOnce("Server_Malfurion_copy", testresult_fixed)
+    print(writeCharNamesAtOnce("Server_Malfurion_copy", testresult_fixed))
+    
+    testset = []
+    for i in range(20000):
+        testset.append(("Char_" + str(i), "Server1"))
+        
+    #writeCharNames("Server_Malfurion_copy", [("Char_300", "Server1"), ("Char_600", "Server1"), ("Char_900", "Server1")])
+    print(writeCharNames("Server_Malfurion_copy", testset))
+    stmt = delete(table)
+    connection.execute(stmt)
+    print(writeCharNamesAtOnce("Server_Malfurion_copy", testresult_fixed))
+    print(writeCharNamesAtOnce("Server_Malfurion_copy", testset))
 
     print("done.")
