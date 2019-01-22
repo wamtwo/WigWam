cff91dbe0b4789b4744a8675771d067b77609500
Daniel Fitzek <Daniel.Fitzek@combionic.com>
2019-01-22 15:02:57 +0100

    Added variable chunks to writeCharNamesAtOnce() and changed engine to LAN DB

--- WigWam/databaseCalls.py
+++ WigWam/databaseCalls.py
@@ -7,13 +7,14 @@ from sqlalchemy.exc import IntegrityError
 
 username = os.environ.get("WIGWAMUSER")
 password = os.environ.get("WIGWAMPW")
-engine = create_engine("mssql+pyodbc://" + username + ":" + password + "@wigwam.database.windows.net/BzApiDB?driver=SQL+Server+Native+Client+11.0")
+#engine = create_engine("mssql+pyodbc://" + username + ":" + password + "@wigwam.database.windows.net/BzApiDB?driver=SQL+Server+Native+Client+11.0")
+engine = create_engine("mssql+pyodbc://" + username + ":" + password + "@192.168.134.122/BzApiDB?driver=SQL+Server+Native+Client+11.0")
 
 metadata = MetaData()
 connection = engine.connect()
 
 def writeCharNames(target_table, namelist): #writing every single entry with commit into DB. Low performance, high error tolerance.
-    table = Table(target_table, metadata, autoload=True, autoload_with=engine)
+    table = Table(target_table, metadata, autoload=True, autoload_with=engine2)
     countrejected = 0
     starttime = time.clock()
     print("Writing to Database Table \"" + target_table + "\"")
@@ -36,8 +37,8 @@ def writeCharNames(target_table, namelist): #writing every single entry with com
     return "{} entries given. {} entries rejected.".format(len(namelist), countrejected)
 
 
-def writeCharNamesAtOnce(target_table, namelist, verbosity=False): # writing all entries at once. Higher performance, low/no error tolerance
-
+def writeCharNamesAtOnce(target_table, namelist, verbosity=False, chunks=10): # writing all entries at once. Higher performance, low/no error tolerance
+    if chunks > 1000 or chunks < 1: return "Chunksize error: Chunksize can only be between 1 and 1000"
     print("Writing to Database Table \"" + target_table + "\"")
 
     table = Table(target_table, metadata, autoload=True, autoload_with=engine)
@@ -56,8 +57,8 @@ def writeCharNamesAtOnce(target_table, namelist, verbosity=False): # writing all
         if tuple not in compareset: namelist_cleaned.append(tuple)
         else: countrejected += 1
   
-    if verbosity == True and len(namelist_cleaned) >= 10:
-        splitlist = np.array_split(namelist_cleaned,10)
+    if verbosity == True and len(namelist_cleaned) >= chunks:
+        splitlist = np.array_split(namelist_cleaned,chunks)
 
         i = 0
         for list in splitlist:
@@ -71,8 +72,8 @@ def writeCharNamesAtOnce(target_table, namelist, verbosity=False): # writing all
                 except:
                     print("Exception occurred")
                     raise
-            i += 10    
-            print(" -> {}% completed".format(i), end="")
+            i += 100 / chunks    
+            print(" -> {:.1f}% completed".format(i), end="")
             if i < 100: print("\r", end="")
             else: print("")
             entries = []
@@ -91,6 +92,7 @@ def writeCharNamesAtOnce(target_table, namelist, verbosity=False): # writing all
 
     elapsed = (time.clock() - starttime)
     #print("Done. \t --- \t %.2f seconds." %elapsed, end="\n\n")
+    connection.close()
     return "Done. {} Entries given. Rejections: {} \t --- \t {:.2f} seconds".format(len(namelist), countrejected, elapsed)
 
 
@@ -110,14 +112,13 @@ def getCharNames(target_table, amount=0):
 
 if __name__ == "__main__":
 
-    testresult = getCharNames("Server_Example", 1912)
+    testresult = getCharNames("Server_Blackmoore", 0)
 
-    #table = Table("Server_Malfurion", metadata, autoload=True, autoload_with=engine)   
-    #stmt = delete(table)
-    #connection.execute(stmt)
+    table = Table("Server_Blackmoore", metadata, autoload=True, autoload_with=engine)   
 
-    print(writeCharNamesAtOnce("Server_Malfurion_copy", testresult, verbosity=True))
-    
+    print("Writing to LAN DB")
+    print(writeCharNamesAtOnce("Server_Blackmoore", testresult, verbosity=True, chunks=100))
+
     #testset = []
     #for i in range(20000):
     #    testset.append(("Char_" + str(i), "Server1"))
